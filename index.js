a1833f782dfd18b9aaa0e4298e7051e51a306bdf
diegolperez <61952943+diegolperez@users.noreply.github.com>
2020-09-04 10:13:59 -0500

    Develop Requirement
    Develop Requirement

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const basicAuth = require('express-basic-auth')
 //local dependencies
 const file = require('./lib/file');
 const converter = require('./lib/converter');
+const filter = require('./lib/filter');
+const utils = require('./lib/utils');
 
 //configuration
 const app = express();
@@ -19,6 +21,8 @@ app.use(basicAuth({
     'usuarioA@hotmail.com': '1234567890',
     'usuarioB@hotmail.com': '1234567890',
     'usuarioB@yahoo.com': '1234567890',
+    'usuarioB@live.com': '1234567890',
+    'usuarioC@ppm.com.ec': '1234567890',
   },
   unauthorizedResponse: (req) => {
     return req.auth ? 'unauthorized' : 'empty credentials'
@@ -26,15 +30,46 @@ app.use(basicAuth({
 }))
 
 app.get('/', (req, res) => {
-  res.send("ok")
+    res.send("ok")
 })
 
 app.get('/data', (req, res) => {
-  const data = file.LoadFromLocal()
-  const dataJSON = converter.FromYaml2JSON(data)
-  res.json(dataJSON);
+    
+ 
+    /*
+     * Filters by Email, Group and Service
+     * Author: Diego Perez 
+     */
+
+    /* 
+     * Get email of Auth Headers, groupFilter and serviceFilter from GET URL
+     * Example of GET web service URL valid filters:
+     * http://localhost:3000/data
+     * http://localhost:3000/data?groupFilter=1
+     * http://localhost:3000/data?groupFilter=1&serviceFilter=servicioA
+     * http://localhost:3000/data?serviceFilter=servicioA
+     */
+    
+    var emailAuth = req.auth.user;
+    var groupFilter = req.query.groupFilter;
+    var serviceFilter = req.query.serviceFilter;
+
+    //Get file depends of groupFilter. If groupFilter not set, by default search in group1 file.
+    const data = file.LoadFromLocal(groupFilter);
+    const dataJSON = converter.FromYaml2JSON(data);
+
+    
+    var dataJSONAux = [];
+    if(typeof serviceFilter !== 'undefined'){
+        //If serviceFilter is specified and set, filter for serviceName too
+        dataJSONAux = filter.ByEmailAndService(dataJSON, emailAuth, serviceFilter);
+    }else{  
+        dataJSONAux = filter.ByEmailAndService(dataJSON, emailAuth);
+    } 
+    res.json(dataJSONAux);
+   
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
